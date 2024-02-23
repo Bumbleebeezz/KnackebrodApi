73154c92b97a3ad4482f2ff25b1221931bafb590
Sebastian Moya <sebastianmoya@live.se>
2024-02-23 10:41:52 +0100

    sebasdtian

--- KnackebrodApi/Program.cs
+++ KnackebrodApi/Program.cs
@@ -1,3 +1,6 @@
+using KnackebrodApi.DataAccess;
+using KnackebrodApi.DataAccess.Entities;
+
 namespace KnackebrodApi
 {
     public class Program
@@ -5,9 +8,33 @@ namespace KnackebrodApi
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddDbContext<KnackeBrodDbContext>();
+
+            builder.Services.AddScoped<StudentRepository>();
+
+            builder.Services.AddScoped<TeacherRepository>();
+
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            app.MapGet("/students", (StudentRepository repo) =>
+            {
+                repo.GetAllStudents();
+            });
+            app.MapGet("/students/{id}", (StudentRepository repo, int id) =>
+            {
+               var results = repo.GetStudentId(id);
+               if (results is null)
+               {
+                  return Results.BadRequest("Bananaws");
+               }
+
+               return Results.Ok(results);
+            });
+            app.MapPost("/studentpost", (StudentRepository repo, Student student) =>
+            {
+
+            });
 
             app.Run();
         }
